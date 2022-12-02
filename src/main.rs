11f5367e51a00c6511d02de431a9e981e7662911
桔右 <chenweitech@qq.com>
2022-12-02 23:28:02 +0800

    refactor: add create_vm

--- src/main.rs
+++ src/main.rs
@@ -24,17 +24,21 @@ fn main() {
             }
         };
 
-        // init v8 scope
-        let isolate = &mut v8::Isolate::new(Default::default());
-        let scope = &mut v8::HandleScope::new(isolate);
-        let context = v8::Context::new(scope);
-        let scope = &mut v8::ContextScope::new(scope, context);
-
-        // excute code
-        let code = v8::String::new(scope, &file_content).unwrap();
-        let script = v8::Script::compile(scope, code, None).unwrap();
-        let result = script.run(scope).unwrap();
-        let result = result.to_string(scope).unwrap();
-        println!("result: {}", result.to_rust_string_lossy(scope));
+        let result = create_vm(file_content);
+        println!("result: {}", result);
     }
+}
+
+fn create_vm(filecontent: String) -> String {
+    // init v8 scope
+    let isolate = &mut v8::Isolate::new(Default::default());
+    let scope = &mut v8::HandleScope::new(isolate);
+    let context = v8::Context::new(scope);
+    let scope = &mut v8::ContextScope::new(scope, context);
+    // excute code
+    let code = v8::String::new(scope, &filecontent).unwrap();
+    let script = v8::Script::compile(scope, code, None).unwrap();
+    let result = script.run(scope).unwrap();
+    let result = result.to_string(scope).unwrap();
+    result.to_rust_string_lossy(scope)
 }
